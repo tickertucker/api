9fb76a243f2970a01b93d31dd0992b0e535734de
unknown <tickertucker007@gmail.com>
2020-07-02 20:57:20 +0800

    Product update

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,11 +2,16 @@
 
 namespace App\Model;
 
+// use app\Model\Review;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     //
+    protected $fillable = [
+        'name','detail','stock','price','discount'
+    ];
     public function reviews()
     {
         return $this->hasMany(Review::class);
